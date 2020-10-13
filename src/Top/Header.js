b17c8741601cccd32df7d3693385fa00af71e4b2
dbworud1234 <dbworud1234@naver.com>
2020-10-13 19:40:38 +0900

    test06

--- src/Top/Header.js
+++ src/Top/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import img from '../images.walk.png';
 
 function Header() {
     return (
         <Container>
             <Element>
                 <span background-size = "contain">
-                    <img src = '/images/walk.png' alt="walk" height = "400px" width = "1080px" resize = "both"/>
+                    <img src = {img} alt="walk" height = "400px" width = "1080px" resize = "both"/>
                 </span>
             </Element>
         </Container>
