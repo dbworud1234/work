8602ae088c61b827fc79a5b8216ba71702781d75
dbworud1234 <dbworud1234@naver.com>
2020-10-13 20:52:52 +0900

    사진2

--- src/Top/Header.js
+++ src/Top/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <Container>
             <Element>
                 <span background-size = "contain">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvB5A3MB176zW31hG6-lVpwwoC0TKvKK0EKA&usqp=CAU" alt="walk" height = "400px" width = "1080px" resize = "both"/>
+                    <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvB5A3MB176zW31hG6-lVpwwoC0TKvKK0EKA&usqp=CAU'} alt="walk" height = "400px" width = "1080px" resize = "both"/>
                 </span>
             </Element>
         </Container>
